39bb55e2b2b6b11da7640451312bbce8a1a442e9
muhammad2341 <125587055+muhammad2341@users.noreply.github.com>
2025-02-17 22:40:40 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/index.jsx";
-import WDW12 from "./WDW tugas/WDW12.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
