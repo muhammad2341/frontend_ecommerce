406ef3717118753fa794b3d0a17e063c6205a03d
muhammad2341 <125587055+muhammad2341@users.noreply.github.com>
2025-02-17 23:50:41 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,15 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 function App() {
-  React.useEffect(() => {
-    AOS.init({
-      offset: 100,
-      duration: 800,
-      easing: "ease-in-out",
-      delay: 100,
-    });
+React.useEffect(() => {
+  AOS.init({
+    offset: 100,
+    duration: 800,
+    easing: "ease-in-out",
+    delay: 100,
   });
+}, []); // Dependency array agar hanya berjalan sekali
+
 
   return (
     <Router> {/* Tambahkan BrowserRouter di sini */}
